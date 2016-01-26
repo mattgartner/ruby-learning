ee9a8db8ae19d31f5eec7d32b1e24c05262913e5
Matt Gartner <mattgartner@gmail.com>
2016-01-25 23:07:54 -0500

    adding items to list

--- shopping_list.rb
+++ shopping_list.rb
@@ -17,7 +17,26 @@ print "What is the item called? "
   return hash
 end
 
-list = create_list
-puts list.inspect
+def print_separator(character="-")
+  puts character * 80
+end
 
-puts add_list_item().inspect
+def print_list(list)
+  puts "List: #{list['name']}"
+  print_separator()
+
+  puts "\tItems: \t\t\t Quantity:"
+
+  list ["items"].each do |item|
+    puts "\t" + item['name'] + "\t\t\t\t" +
+         item['quantity'].to_s
+    
+  end
+  print_separator()
+end
+
+list = create_list
+5.times {list['items'].push(add_list_item()) }
+#list['items'].push(add_list_item())
+
+print_list(list)

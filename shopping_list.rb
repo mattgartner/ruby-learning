d73b58c31da1a9c3ab7f789981259cb36c0c2ddb
Matt Gartner <mattgartner@gmail.com>
2016-01-25 22:39:15 -0500

    update

--- shopping_list.rb
+++ shopping_list.rb
@@ -7,12 +7,17 @@ def create_list
 end
 
 def add_list_item
-print "What is the item called?"
+print "What is the item called? "
   item_name = gets.chomp
   
-  hash = { "name" => name }
+  print "How much? "
+  quantity = gets.chomp.to_i
+  
+  hash = { "name" => item_name, "quantity" => quantity }
   return hash
 end
 
 list = create_list
 puts list.inspect
+
+puts add_list_item().inspect

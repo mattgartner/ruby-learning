5e4b3db1d3969a398746baf55dc6b1f85ecc94e4
Matt Gartner <mattgartner@gmail.com>
2016-01-26 21:29:09 -0500

    while loop with user entry

--- while_loop.rb
+++ while_loop.rb
@@ -1,6 +1,22 @@
 #simple while loop
-answer = ""
-while answer != "n"
-  print "Do you want me to repeat this loop? (y/n) "
-  answer = gets.chomp.downcase
+
+#answer = ""
+#while answer != "n"
+#  print "Do you want me to repeat this loop? (y/n) "
+#  answer = gets.chomp.downcase
+#end
+
+def print_hello(times)
+  i = 0
+  while i < times
+    puts "hello"
+    i += 1
+  end
+end
+
+answer = 0
+while answer < 5
+  print "How many times should I say hello? (enter a number > 5 to exit) "
+  answer = gets.chomp.to_i
+  print_hello(answer)
 end
